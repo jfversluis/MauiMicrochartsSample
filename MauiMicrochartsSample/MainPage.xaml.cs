0f441aa05b683ca4c4722ddd32fb47f96bf2fdca
Gerald Versluis <gerald.versluis@microsoft.com>
2023-08-07 15:24:45 +0200

    Add Microcharts

--- MauiMicrochartsSample/MainPage.xaml.cs
+++ MauiMicrochartsSample/MainPage.xaml.cs
@@ -1,24 +1,51 @@
-﻿namespace MauiMicrochartsSample
+﻿using Microcharts;
+using SkiaSharp;
+
+namespace MauiMicrochartsSample
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        ChartEntry[] entries = new[]
+        {
+            new ChartEntry(212)
+            {
+                Label = "Windows",
+                ValueLabel = "112",
+                Color = SKColor.Parse("#2c3e50")
+            },
+            new ChartEntry(248)
+            {
+                Label = "Android",
+                ValueLabel = "648",
+                Color = SKColor.Parse("#77d065")
+            },
+            new ChartEntry(128)
+            {
+                Label = "iOS",
+                ValueLabel = "428",
+                Color = SKColor.Parse("#b455b6")
+            },
+            new ChartEntry(514)
+            {
+                Label = ".NET MAUI",
+                ValueLabel = "214",
+                Color = SKColor.Parse("#3498db")
+            }
+        };
 
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+            chartView.Chart = new BarChart
+            {
+                Entries = entries
+            };
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            chartView1.Chart = new LineChart
+            {
+                Entries = entries
+            };
         }
     }
 }
--- MauiMicrochartsSample/MauiProgram.cs
+++ MauiMicrochartsSample/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microcharts.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace MauiMicrochartsSample
 {
@@ -9,6 +10,7 @@ namespace MauiMicrochartsSample
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMicrocharts()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
